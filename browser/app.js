aee3770fc7c2324693cfe10421f2ada90f10252d
arian457 <82475239+arian457@users.noreply.github.com>
2021-06-25 17:44:00 -0300

    Add files via upload
    run npm install to node_modules

---
+++ browser/app.js
@@ -0,0 +1,30 @@
+const { socketio } = require("../server.js");
+const { whiteboard } = require("./whiteboard.js");
+
+
+  var socket = socketio;
+
+  socket.on('connect', function () {
+    console.log('Connected!');
+  });
+
+  socket.on('load', function (strokes) {
+
+    strokes.forEach(function (stroke) {
+      var start = stroke.start;
+      var end = stroke.end;
+      var color = stroke.color;
+      whiteboard.draw(start, end, color, false);
+    });
+
+  });
+
+  socket.on('draw', function (start, end, color) {
+    whiteboard.draw(start, end, color, false);
+  });
+
+  whiteboard.on('draw', function (start, end, color) {
+    socket.emit('draw', start, end, color);
+  });
+
+
